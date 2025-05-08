edc1957f7c844bdb37f456476ca1a50ad49b7887
liam-handasyde <lhandasyde@anzca.edu.au>
2025-05-09 09:45:19 +1000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ * Refs: 
+ * https://www.c-sharpcorner.com/blogs/fetching-secrets-from-key-vault-in-net-console-app
+ * A bit of Copilot help 
+ */
+using KeyVault_POC.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((context, config) =>
+    {
+        // I can read both, environment variables and user secrets without enabling these two:
+        config.AddEnvironmentVariables();
+        config.AddUserSecrets<Program>();
+
+        var environment = context.HostingEnvironment;
+        Console.WriteLine($"Environment: {environment.EnvironmentName}");
+        if (environment.IsDevelopment())
+        {
+            Console.WriteLine("For when you run from the IDE - Development environment");
+            var projectPath = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName).FullName;
+            config.SetBasePath(projectPath);
+        }
+        else
+        {
+            Console.WriteLine("For when you run from the CLI - Not development environment");
+            config.SetBasePath(Directory.GetCurrentDirectory());
+        }
+
+        //  Pick up configuration settings.
+        config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+    })
+    .ConfigureServices((hostContext, services) =>
+    {
+        services.AddLogging();
+
+        services.AddTransient<AzureKeyVaultService>();
+    })
+    .Build();
+
+var azureKeyVaultService = host.Services.GetRequiredService<AzureKeyVaultService>();
+
+Console.WriteLine("\nAzure Key Vault - secrets\n");
+Console.WriteLine($"POC Secret 1: {await azureKeyVaultService.GetSecretAsync("Key-One")}");
+Console.WriteLine($"POC Secret 2: {await azureKeyVaultService.GetSecretAsync("Key-Two")}");
+
+Console.WriteLine("\nHello, Azure Key Vault POC!");
+Console.ReadKey();
---
+++ Services/AzureKeyVaultService.cs
@@ -0,0 +1,35 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyVault_POC.Services
+{
+    public class AzureKeyVaultService(IConfiguration configuration)
+    {
+        private readonly IConfiguration _configuration = configuration;
+        private readonly string _clientId = configuration["AzureKeyVault:ClientId"];
+        private readonly string _clientSecret = configuration["AzureKeyVault:ClientSecret"];
+        private readonly string _tenantId = configuration["AzureKeyVault:TenantId"];
+        private readonly Uri _keyVaultUri = new Uri($"https://{configuration["AzureKeyVault:Uri"]}.vault.azure.net/");
+
+        /// <summary>
+        /// Gets an Azure Key Vault secret by the specified Key. Implements Client Credential Flow, not tied to an individual user's credentials.
+        /// </summary>
+        /// <param name="key">The specific secret you require access to</param>
+        /// <returns>Nullable string</returns>
+        public async Task<string?> GetSecretAsync(string key)
+        {
+            var credential = new ClientSecretCredential(_tenantId, _clientId, _clientSecret);
+            var secretClient = new SecretClient(_keyVaultUri, credential);
+
+            var keyVaultSecret = (await secretClient.GetSecretAsync(key)).Value;
+
+            return keyVaultSecret?.Value;
+        }
+    }
+}
